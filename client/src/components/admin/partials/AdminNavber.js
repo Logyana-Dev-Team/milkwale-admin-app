52c808354adfa26e6096bd57d384bc997c783d5f
noob69-dot <arjunople@gmail.com>
2021-02-08 11:58:39 +0530

    subscription frontend done

--- client/src/components/admin/partials/AdminNavber.js
+++ client/src/components/admin/partials/AdminNavber.js
@@ -17,7 +17,7 @@ const AdminNavber = (props) => {
         {/*  Large Screen Show  */}
         <div className="hidden lg:block lg:flex lg:items-center lg:space-x-4 mr-32">
           <span>
-            <svg
+            {/* <svg
               className="w-8 h-8 cursor-pointer text-gray-600"
               fill="none"
               stroke="currentColor"
@@ -30,7 +30,7 @@ const AdminNavber = (props) => {
                 strokeWidth={2}
                 d="M4 6h16M4 12h16M4 18h16"
               />
-            </svg>
+            </svg> */}
           </span>
         </div>
         {/*  Large Screen Show  */}
@@ -38,7 +38,8 @@ const AdminNavber = (props) => {
           <span
             onClick={(e) => history.push("/admin/dashboard")}
             style={{ letterSpacing: "0.70rem" }}
-            className="flex items-left text-center font-bold uppercase text-gray-800 text-2xl cursor-pointer px-2 text-center"
+            className="flex items-left text-center font-bold uppercase text-gray-800 
+            text-2xl cursor-pointer px-2 text-center"
           >
             Milkwale
           </span>
@@ -70,7 +71,7 @@ const AdminNavber = (props) => {
         </div>
         {/* Both Screen show */}
         <div className="flex items-center">
-          <div className="hover:bg-gray-200 rounded-lg p-2" title="Search">
+          {/* <div className="hover:bg-gray-200 rounded-lg p-2" title="Search">
             <svg
               className="cursor-pointer w-8 h-8 text-gray-600 hover:text-gray-800"
               fill="none"
@@ -85,8 +86,8 @@ const AdminNavber = (props) => {
                 d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
               />
             </svg>
-          </div>
-          <div className="hover:bg-gray-200 rounded-lg p-2" title="Search">
+          </div> */}
+          {/* <div className="hover:bg-gray-200 rounded-lg p-2" title="Search">
             <svg
               className="cursor-pointer w-8 h-8 text-gray-600 hover:text-gray-800"
               fill="none"
@@ -101,7 +102,7 @@ const AdminNavber = (props) => {
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
             </svg>
-          </div>
+          </div> */}
           {/* Logout Button Dropdown */}
           <div
             className="userDropdownBtn hover:bg-gray-200 px-2 py-2 rounded-lg relative"
@@ -123,7 +124,7 @@ const AdminNavber = (props) => {
             </svg>
             <div className="userDropdown absolute right-0 mt-1 bg-gray-200 rounded">
               <li className="flex flex-col text-gray-700">
-                <span
+                {/* <span
                   onClick={(e) => history.push("/")}
                   className="flex space-x-1 py-2 px-8 hover:bg-gray-400 cursor-pointer"
                 >
@@ -144,7 +145,7 @@ const AdminNavber = (props) => {
                     </svg>
                   </span>
                   <span>Shop</span>
-                </span>
+                </span> */}
                 <span className="flex space-x-1 py-2 px-8 hover:bg-gray-400 cursor-pointer">
                   <span>
                     <svg
